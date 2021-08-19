704eaeb1fe1f629626a7fcae80991a7a325a04cb
marshall-johnson <marshalljohnsonp@gmail.com>
2021-08-19 22:07:35 +0700

    Update ortho submit data doctor production page - Fetch doctor name from API - Validate input fields

--- src/containers/Ortho/SubmitData/DoctorProductionStep/index.js
+++ src/containers/Ortho/SubmitData/DoctorProductionStep/index.js
@@ -1,24 +1,120 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import {
   PlusOutlined
 } from '@ant-design/icons';
+import {
+  forEach,
+} from 'lodash';
 import {
   Row,
   Col,
   Form,
   Input,
-  Select,
   Button,
   Divider,
   PageHeader,
 } from 'antd';
+import camelcaseKeys from 'camelcase-keys';
 
+import api from '@/api';
+import DebounceSelect from '@/components/DebounceSelect';
 import AppConfig from '@/constants/AppConfig';
+import {
+  fetchDoctors,
+} from '@/actions/doctorActions';
+
+const validateMessages = {
+  // eslint-disable-next-line no-template-curly-in-string
+  required: '${label} is required!',
+};
 
 class DoctorProductionStep extends Component {
+  formRef = React.createRef();
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialValues: {
+        doctorProduction: [
+          {
+            name: null,
+            drProduction: null,
+            productionAdj: null,
+            netProduction: null,
+            hoursAvailable: null,
+            hoursSchedule: null,
+            startApptsAvailable: null,
+            patientVisits: null,
+            apptChanges: null,
+          }
+        ],
+      }
+    };
+  }
+
+  componentDidMount() {
+    const { fetchDoctors, page } = this.props;
+    const formData = JSON.parse(localStorage.getItem('orthoDoctorProduction'));
+
+    fetchDoctors({ page });
+
+    this.formRef.current.setFieldsValue(formData);
+
+    window.onbeforeunload = (e) => {
+      localStorage.removeItem('orthoDoctorProduction');
+    };
+  }
+
+  fetchDoctorList = (keyword) => api
+    .get('/api/v1/doctors', { params: { search: keyword } })
+    .then(({ data: response }) => camelcaseKeys(response, { deep: true }))
+    .then(({ result }) => result.data.map((item) => ({
+      label: item.attributes.fullname,
+      value: item.id,
+    })))
+    .catch((error) => {
+      throw error;
+    })
+
+  optionInit = () => {
+    const {
+      items
+    } = this.props;
+
+    const data = [];
+
+    forEach(items, item => {
+      const {
+        fullname,
+      } = item.attributes;
+
+      data.push({
+        label: fullname,
+        value: item.id,
+      });
+    });
+
+    return data;
+  }
+
+  onFinish = data => {
+    localStorage.setItem('orthoDoctorProduction', JSON.stringify(data));
+
+    const { history } = this.props;
+    history.push(`${AppConfig.ROUTES.ORTHO}/${AppConfig.ORTHO_SUBMIT_DATA_STEPS.COLLECTIONS}`);
+  }
+
   render() {
+    const {
+      initialValues,
+    } = this.state;
+
     return (
-      <>
+      <div className="ortho-doctor-production-container">
         <PageHeader
           className="site-page-header"
           title="Ortho Submit Data"
@@ -28,95 +124,173 @@ class DoctorProductionStep extends Component {
         <Divider />
 
         <Form
-          labelCol={{
-            span: 9,
-          }}
+          ref={this.formRef}
           layout="horizontal"
+          labelCol={{ span: 9 }}
+          onFinish={this.onFinish}
+          initialValues={initialValues}
+          validateMessages={validateMessages}
         >
           <Row gutter={[32, 16]}>
-            <Col span={12}>
-              <Form.Item label="Doctor Name">
-                <Select>
-                  <Select.Option value="Doctor 01">Doctor 01</Select.Option>
-                  <Select.Option value="Doctor 02">Doctor 02</Select.Option>
-                </Select>
-              </Form.Item>
-              <Form.Item label="Dr. Production">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Production Adj">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Net Production">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Dr Patient Hours Available">
-                <Input />
-              </Form.Item>
-              <Form.Item label="Dr. Patient Hours Schedule w/ Pts">
-                <Input />
-              </Form.Item>
-              <Form.Item label="# of Start Appts Available">
-                <Input />
-              </Form.Item>
-              <Form.Item label="# of Start Appts Available">
-                <Input />
-              </Form.Item>
-              <Form.Item label="# of Patient Visits">
-                <Input />
-              </Form.Item>
-              <Form.Item label="# of Appt Changes">
-                <Input />
-              </Form.Item>
-            </Col>
-            <Form.List name="doctor">
+            <Form.List name="doctorProduction">
               {(fields, { add, remove }) => (
                 <>
                   {fields.map((field) => (
                     <Col span={12} key={field.key}>
-                      <Form.Item label="Doctor Name">
-                        <Select>
-                          <Select.Option value="Doctor 01">Doctor 01</Select.Option>
-                          <Select.Option value="Doctor 02">Doctor 02</Select.Option>
-                        </Select>
+                      <Form.Item
+                        label="Doctor Name"
+                        rules={[{ required: true }]}
+                        name={[field.name, 'name']}
+                        fieldKey={[field.fieldKey, 'name']}
+                      >
+                        <DebounceSelect
+                          showSearch
+                          optionInit={this.optionInit()}
+                          placeholder="Select Doctor"
+                          fetchOptions={this.fetchDoctorList}
+                          style={{ width: '100%' }}
+                        />
                       </Form.Item>
-                      <Form.Item label="Dr. Production">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="Production Adj">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="Net Production">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="Hours Available">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="Hours Schedule w/ Pts">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="# of Start Appts Available">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="# of Start Appts Available">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="# of Patient Visits">
-                        <Input />
-                      </Form.Item>
-                      <Form.Item label="# of Appt Changes">
+                      <Form.Item
+                        label="Dr. Production"
+                        name={[field.name, 'drProduction']}
+                        fieldKey={[field.fieldKey, 'drProduction']}
+                        rules={[{ required: true }]}
+                      >
                         <Input />
                       </Form.Item>
                       <Form.Item
-                        style={{ textAlign: 'right' }}
+                        label="Production Adj"
+                        name={[field.name, 'productionAdj']}
+                        fieldKey={[field.fieldKey, 'productionAdj']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                        ]}
                       >
-                        <Button
-                          onClick={() => remove(field.name)}
-                          type="danger"
-                        >
-                          Remove
-                        </Button>
+                        <Input />
                       </Form.Item>
+                      <Form.Item
+                        label="Net Production"
+                        name={[field.name, 'netProduction']}
+                        fieldKey={[field.fieldKey, 'netProduction']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('Net Production is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      <Form.Item
+                        label="Dr Patient Hours Available"
+                        name={[field.name, 'hoursAvailable']}
+                        fieldKey={[field.fieldKey, 'hoursAvailable']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('Dr Patient Hours Available is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      <Form.Item
+                        label="Dr. Patient Hours Schedule w/ Pts"
+                        name={[field.name, 'hoursSchedule']}
+                        fieldKey={[field.fieldKey, 'hoursSchedule']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('Dr. Patient Hours Schedule w/ Pts is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      <Form.Item
+                        label="# of Start Appts Available"
+                        name={[field.name, 'startApptsAvailable']}
+                        fieldKey={[field.fieldKey, 'startApptsAvailable']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('# of Start Appts Available is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      <Form.Item
+                        label="# of Patient Visits"
+                        name={[field.name, 'patientVisits']}
+                        fieldKey={[field.fieldKey, 'patientVisits']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('# of Patient Visits is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      <Form.Item
+                        label="# of Appt Changes"
+                        name={[field.name, 'apptChanges']}
+                        fieldKey={[field.fieldKey, 'apptChanges']}
+                        rules={[
+                          {
+                            required: true,
+                          },
+                          {
+                            validator: (_, value) =>
+                              !isNaN(value) ?
+                                Promise.resolve() :
+                                Promise.reject(new Error('# of Appt Changes is not a valid number'))
+                          },
+                        ]}
+                      >
+                        <Input />
+                      </Form.Item>
+                      {field.key > 0 &&
+                        <Form.Item
+                          style={{ textAlign: 'right' }}
+                        >
+                          <Button
+                            onClick={() => remove(field.name)}
+                            type="danger"
+                          >
+                            Remove
+                          </Button>
+                        </Form.Item>
+                      }
                     </Col>
                   ))}
 
@@ -143,17 +317,35 @@ class DoctorProductionStep extends Component {
                 Back
               </Button>
               <Button
-                href={`${AppConfig.ROUTES.ORTHO}/${AppConfig.ORTHO_SUBMIT_DATA_STEPS.COLLECTIONS}`}
                 type="primary"
+                htmlType="submit"
               >
                 Next
               </Button>
             </Col>
           </Row>
         </Form>
-      </>
+      </div>
     );
   }
 }
 
-export default DoctorProductionStep;
+DoctorProductionStep.propTypes = {
+  fetchDoctors: PropTypes.func,
+  items: PropTypes.array,
+  page: PropTypes.number,
+  history: PropTypes.object,
+};
+
+const mapStateToProps = ({ doctor, error }) => ({
+  items: doctor.items,
+  totalCount: doctor.totalCount,
+  page: doctor.page,
+  loading: doctor.loading,
+});
+
+export default withRouter(
+  connect(mapStateToProps, {
+    fetchDoctors,
+  })(DoctorProductionStep),
+);
