20cf1b39df6545aa5251989d2fbda65ef18db7d7
marshall-johnson <marshalljohnsonp@gmail.com>
2021-11-19 16:04:45 +0700

    feature: update the coaching ppp form can be editable

--- src/containers/Coaching/CoachingPPP/tab1.js
+++ src/containers/Coaching/CoachingPPP/tab1.js
@@ -1,34 +1,89 @@
 import React, { Component } from 'react';
 import './index.scss';
+import { Button, Form } from 'antd';
 import { tabsleft, tabsright } from './config';
 import TabSummary from '../tab1/tabSummary';
 
 class Tab1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: {},
+      totalTabLeft: 0,
+      totalTabRight: 0,
+    };
+  }
+
+  handleTotal = (_, value) => {
+    const total = {};
+    Object.keys(value).forEach((field) => {
+      const fieldObject = value[field];
+      total[field] = Object.keys(fieldObject).reduce(
+        (previous, value) => previous + (Number(fieldObject[value]) || 0),
+        0,
+      );
+    });
+
+    this.setState({
+      total,
+      totalTabLeft: tabsleft.reduce(
+        (previous, tab) => previous + (total[tab.key] || 0),
+        0,
+      ),
+      totalTabRight: tabsright.reduce(
+        (previous, tab) => previous + (total[tab.key] || 0),
+        0,
+      ),
+    });
+  };
+
   render() {
+    const { total, totalTabLeft, totalTabRight } = this.state;
+
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}
+      <Form
+        className="form-wrapper"
+        name="data"
+        onValuesChange={this.handleTotal}
+        autoComplete="off"
       >
-        <div style={{ width: '100%', marginRight: '50px' }}>
-          <TabSummary
-            tabData={tabsleft}
-            totalData={[{ label: 'Total', value: 0 }]}
-          />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <div style={{ width: '100%', marginRight: '50px' }}>
+            <TabSummary
+              total={total}
+              tabData={tabsleft}
+              totalData={[{ label: 'Total', value: totalTabLeft }]}
+            />
+          </div>
+          <div style={{ width: '100%' }}>
+            <TabSummary
+              total={total}
+              tabData={tabsright}
+              totalData={[
+                { label: 'IRA', value: 0 },
+                { label: 'Total', value: totalTabRight },
+              ]}
+            />
+          </div>
         </div>
-        <div style={{ width: '100%' }}>
-          <TabSummary
-            tabData={tabsright}
-            totalData={[
-              { label: 'IRA', value: 0 },
-              { label: 'Total', value: 0 },
-            ]}
-          />
-        </div>
-      </div>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{ marginRight: 20, marginTop: 50 }}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
--- src/containers/Coaching/tab1/tabSummary.js
+++ src/containers/Coaching/tab1/tabSummary.js
@@ -1,63 +1,121 @@
 import React, { Component } from 'react';
-import { Row, Col, Typography, Collapse } from 'antd';
+import { Row, Col, Typography, Collapse, Form, Input } from 'antd';
 import PropTypes from 'prop-types';
-import { CaretRightOutlined } from '@ant-design/icons';
-import { data } from '../CoachingPPP/mock';
+import { CaretRightOutlined, PlusCircleFilled } from '@ant-design/icons';
+import configColumn from '../CoachingPPP/configColumn';
 
 const { Panel } = Collapse;
 const { Text } = Typography;
 
 class TabSummary extends Component {
   render() {
-    const { tabData, totalData } = this.props;
+    const { tabData, totalData, total } = this.props;
 
     return (
-      <Collapse
-        bordered={false}
-        defaultActiveKey={['1']}
-        expandIcon={({ isActive }) => (
-          <CaretRightOutlined rotate={isActive ? 90 : 0} />
-        )}
-        className="site-collapse-custom-collapse"
-      >
-        {tabData.map((tab, index) => (
-          <Panel
-            header={tab.name}
-            key={index.toString()}
-            className="site-collapse-custom-panel"
-          >
-            {data[tab.key]?.map((row, index) => (
-              <Row gutter={24} key={tab.name + index.toString()}>
-                <Col span={24}>
-                  <Row style={{ marginBottom: 10 }}>
-                    <Col span={12}>
-                      <Text>{row.label}</Text>
-                    </Col>
-                    <Col span={12} className="border-bottom">
-                      <Text>{row.value}</Text>
-                    </Col>
-                  </Row>
+      <>
+        <Collapse
+          bordered={false}
+          collapsible={false}
+          expandIcon={({ isActive }) => (
+            <CaretRightOutlined rotate={isActive ? 90 : 0} />
+          )}
+          className="site-collapse-custom-collapse"
+        >
+          {tabData.map((tab, index) => (
+            <Panel
+              header={tab.name}
+              key={index.toString()}
+              className="site-collapse-custom-panel"
+            >
+              {configColumn[tab.key]?.map((row, index) => (
+                <Row gutter={24} key={tab.name + index.toString()}>
+                  <Col span={24}>
+                    <Row style={{ marginBottom: 10 }}>
+                      <Col span={12}>
+                        <Text>{row.label}</Text>
+                      </Col>
+                      <Col span={12} className="border-bottom">
+                        <Form.Item
+                          className="input-item"
+                          name={[tab.key, row.key]}
+                          rules={row?.rules}
+                        >
+                          <Input />
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              ))}
+              <Row gutter={24}>
+                <Col span={12}>
+                  <Text
+                    style={{
+                      marginTop: 10,
+                      fontSize: 16,
+                      width: 80,
+                      fontWeight: 'normal',
+                      color: '#349DFF',
+                    }}
+                  >
+                    <PlusCircleFilled
+                      style={{ marginRight: 5, color: '#349DFF' }}
+                    />
+                    Total
+                  </Text>
+                </Col>
+                <Col span={12}>
+                  <Text
+                    style={{
+                      fontSize: 16,
+                      marginTop: 10,
+                      color: '#349DFF',
+                    }}
+                  >
+                    {total[tab.key] || 0}
+                  </Text>
                 </Col>
               </Row>
-            ))}
-          </Panel>
-        ))}
+            </Panel>
+          ))}
+        </Collapse>
 
         {totalData.map((data, index) => (
           <div
             style={{ display: 'flex', flexDirection: 'row' }}
             key={`tab_summary_data_${index.toString()}`}
           >
-            <Text style={{ marginRight: 20 }}>{data.label}</Text>
-            <Text>{data.value}</Text>
+            <Text
+              style={{
+                marginRight: 20,
+                marginTop: 10,
+                fontSize: 16,
+                width: 80,
+                fontWeight: 'normal',
+                color: '#349DFF',
+              }}
+            >
+              <PlusCircleFilled style={{ marginRight: 5, color: '#349DFF' }} />
+              {data.label}
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                marginTop: 10,
+                color: '#349DFF',
+              }}
+            >
+              {data.value}
+            </Text>
           </div>
         ))}
-      </Collapse>
+      </>
     );
   }
 }
 
 TabSummary.propTypes = {
+  total: PropTypes.object,
   tabData: PropTypes.array,
   totalData: PropTypes.array,
 };
